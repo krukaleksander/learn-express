f7c446f48ac6604d85084f81052574639db0804f
Alek Kruk <kruk.aleksander@hotmail.com>
2019-11-12 06:21:26 +0100

    admin ready

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var createError = require('http-errors');
+var cookieSession = require('cookie-session')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+mongoose.connect(config.db, {
+  useNewUrlParser: true
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
@@ -23,6 +33,12 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession
+}))
 
 app.use(function (req, res, next) {
   // przypisujesz do zmiennej globalnej path
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:DkoUy6eWrNBAD8CF@cluster0-bik4j.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['ajaklj187891'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
