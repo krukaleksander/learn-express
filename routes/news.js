59302e393ac3152da3cf6acfc84b6c0f43dd51d4
Alek Kruk <kruk.aleksander@hotmail.com>
2019-11-13 06:01:13 +0100

    news section ready

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
+
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('news', {
-        title: 'News'
-    });
+    // to co znajduje się w query stringu np z zapytania naszej wyszukiwarki
+    // jest pod req.query
+    // dlatego odwołujesz się do req.query.search
+    const search = req.query.search;
+    const findNews = News
+        // szukasz po title a co to podajesz search z consta powyżej
+        // RegExp to klasa z mongoose służy do usprawniania wyszukiwania. Pozwala np aby wyszukiwanie było case insensitive
+        .find({
+            title: new RegExp(search, 'i')
+        })
+        // specjalna metoda do sortowania
+        // -1 malejąco 1 rosnąco
+        .sort({
+            created: -1
+        });
+
+    findNews.exec((err, data) => {
+        res.render('news', {
+            title: 'News',
+            data,
+            search
+        });
+    })
+
 });
 
 module.exports = router;
