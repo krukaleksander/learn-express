f1a79ec8f783182694bb4fc2f53d1b728e547b50
Alek Kruk <kruk.aleksander@hotmail.com>
2019-11-16 10:18:55 +0100

    final commit

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz');
+
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('quiz', {
-        title: 'Quiz'
-    });
-});
+    // show jest do blokowania ponownego głosu!
+    const show = !req.session.vote;
+    Quiz.find({}, (err, data) => {
+        let sum = 0;
+        data.forEach((item) => {
+            sum += item.vote;
+        })
+        res.render('quiz', {
+            title: 'Quiz',
+            data,
+            show,
+            sum
+        });
+    })
 
+});
+router.post('/', (req, res) => {
+    // dlatego na końcu jest quiz bo ustawiliśmy w quiz.pug nazwę radio 
+    // buttona na quiz
+    const id = req.body.quiz;
+    Quiz.findOne({
+        _id: id
+    }, (err, data) => {
+        data.vote = data.vote + 1;
+        data.save((err) => {
+            // zablkowanie oddawania kolejnego głosu
+            req.session.vote = 1;
+            // redirect w save zeby wykonał się wewnątrz funkcji przy zapisie danych.
+            res.redirect('/quiz');
+        });
+
+    })
+
+});
 module.exports = router;
